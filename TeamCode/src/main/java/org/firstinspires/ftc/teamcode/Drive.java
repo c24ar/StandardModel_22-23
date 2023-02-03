6c527a79fb5c44f71d320af9b4202779c5aff127
Jodany <90403528+Jodany@users.noreply.github.com>
2023-02-03 18:03:36 -0500

    stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
@@ -23,8 +24,8 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Basic: Iterative OpMode", group="Iterative Opmode")
-@Disabled
+@TeleOp(name="Drive", group="Iterative Opmode")
+
 public class Drive extends OpMode
 {
     // Declare OpMode members.
@@ -33,8 +34,8 @@ public class Drive extends OpMode
     private DcMotor BackRight;
     private DcMotor FrontLeft;
     private DcMotor FrontRight;
-
-    //private Servo Claw;
+    private DcMotor Slide;
+    private Servo Claw;
     /*
      * Code to run ONCE when the driver hits INIT
      */
@@ -49,22 +50,25 @@ public class Drive extends OpMode
         BackLeft = hardwareMap.get(DcMotor.class, "BackLeft");
         FrontRight = hardwareMap.get(DcMotor.class, "FrontRight");
         BackRight = hardwareMap.get(DcMotor.class, "BackRight");
-        //Claw = hardwareMap.get(Servo.class, "FrontRight");
+        Claw = hardwareMap.get(Servo.class, "Claw");
+        Slide = hardwareMap.get(DcMotor.class, "Slide");
 
 
         // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
         // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
         // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
 
-        FrontLeft.setDirection(DcMotor.Direction.REVERSE);
-        BackLeft.setDirection(DcMotor.Direction.REVERSE);
-        FrontRight.setDirection(DcMotor.Direction.FORWARD);
-        BackRight.setDirection(DcMotor.Direction.FORWARD);
+        FrontLeft.setDirection(DcMotor.Direction.FORWARD);
+        BackLeft.setDirection(DcMotor.Direction.FORWARD);
+        FrontRight.setDirection(DcMotor.Direction.REVERSE);
+        BackRight.setDirection(DcMotor.Direction.REVERSE);
+        Slide.setDirection(DcMotor.Direction.FORWARD);
 
         FrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         BackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         FrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         BackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        Slide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         // Tell the driver that initialization is complete.
         telemetry.addData("Status", "Initialized");
@@ -82,9 +86,13 @@ public class Drive extends OpMode
      */
     @Override
     public void start() {
+        telemetry.addData("Status", "1");
         runtime.reset();
-    }
+        Claw.setPosition(0.7);
+        telemetry.addData("Status", "1");
 
+
+    }
     /*
      * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
      */
@@ -93,15 +101,24 @@ public class Drive extends OpMode
         // Setup a variable for each drive wheel to save power level for telemetry
 
 
+        if (gamepad2.y) {
+            Claw.setPosition(0.3);
+            telemetry.addData("Status", "2");
 
+        }
+        else if (gamepad2.x) {
+            Claw.setPosition(0.7);
+            telemetry.addData("Status", "1");
+
+        }
         // Choose to drive using either Tank Mode, or POV Mode
         // Comment out the method that's not used.  The default below is POV.
 
         // POV Mode uses left stick to go forward, and right stick to turn.
         // - This uses basic math to combine motions and is easier to drive straight.
-        double drive = -gamepad1.left_stick_y;
-        double turn  =  gamepad1.right_stick_x;
-        double strafe = gamepad1.left_stick_x;
+        double strafe = gamepad1.left_stick_y;
+        double turn  =  -gamepad1.right_stick_x;
+        double drive = -gamepad1.left_stick_x;
 
         double frontLeftPower = Range.clip(drive + turn + strafe, -1.0, 1.0) * 0.8;
         double frontRightPower = Range.clip(drive - turn - strafe, -1.0, 1.0) * 0.8;
@@ -116,6 +133,7 @@ public class Drive extends OpMode
         BackLeft.setPower(backLeftPower);
         FrontRight.setPower(frontRightPower);
         BackRight.setPower(backRightPower);
+        Slide.setPower(gamepad2.left_stick_y);
 
         // Show the elapsed game time and wheel power.
         telemetry.addData("Status", "Run Time: " + runtime.toString());
